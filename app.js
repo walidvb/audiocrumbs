401bc3fb177e985a61a88e9b8399ea1c9f2e6e70
walidvb <walidvb@gmail.com>
2018-07-25 21:42:59 +0200

    adds websocket

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var WebSocketServer = require('websocket').server;
 
 var app = express();
 
