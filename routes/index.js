807f514e07a19c95ebb35754945ec7cae3025f5d
walidvb <walidvb@gmail.com>
2018-07-30 21:58:52 +0200

    adds layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {layout: false});
+});
+router.get('/begin', function (req, res, next) {
+  res.render('experience');
 });
 
 module.exports = router;
