2f86cc1389400a1a7058e681e86c59075311c230
walidvb <walidvb@gmail.com>
2018-07-30 21:46:34 +0200

    adds layout

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+expressLayouts = require('express-layouts')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var WebSocketServer = require('websocket').server;
 
 var app = express();
 
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
+app.set('layout', 'layout'); // defaults to 'layout'
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
